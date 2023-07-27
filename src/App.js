c2f4eb1dd5201ce59f29c2213f68978b2f5a5fe7
pratapsingh.bisht <pratapsingh.bisht@greenpointglobal.com>
2023-07-27 14:18:51 +0530

    packages changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
