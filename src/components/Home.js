bf91369579a37be188de7dc2e35051b7e5822db2
namrata.shirsat <namrata.shirsat@greenpoint.com>
2023-07-28 12:10:57 +0530

    remove video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,70 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
 import { FaAngleRight } from "react-icons/fa";
+import VideoPlayer from "react-video";
 
 const Home = () => {
   return (
     <section>
-    <Container fluid id='home-bg'>
-      <Container>
-      <Row>
-        <Col className='col-lg-6 col-md-6 col-sm-12 mt-5'>
-          <h1 className='text-lead'>Modern CNC Solutions</h1>
-          <ul className="styled fs-5">
-            <li>New contemporary high-tech CNC tool machines</li>
-            <li>Globally advanced CAD/CAM software</li>
-            <li>Application of comprehensive design, production and control computerized systems.</li>
-            <li>Creation of a fully equipped production control department</li>
-          </ul>
-        </Col>
-        <Col className='col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 d-none d-sm-block'>
-        <img alt="" src="/images/CNC.jpg" height="100" className="img-fluid rounded-5" />
-        </Col>
-      </Row>
-    </Container>
-    </Container>
-    <Container>
-      <Row>
-      <Col className='col-lg-6 col-md-6 col-sm-12 mt-5 mb-5'>
-        <ReactPlayer url="\videos\cnc-machining-working-videos.mp4" controls={true} width={500}></ReactPlayer>
-        </Col>
-        <Col className='col-lg-6 col-md-6 col-sm-12 mt-5'>
-          <h1 className='text-lead mt-5'>CNC Milling and CNC Turning</h1>
-          <p className='fs-5'>CNC Milling and CNC Turning allow users to create patterns and add detail to metals that are impossible to do by hand. CNC Milling uses commands, codes programmed into the computer and set to run. The mill then drills and turns along axes to cut materials to dimensions entered into the computer.</p>
-        </Col>
-      </Row>
-    </Container>
-    <Container>
-      <Row>
-        <Col className='work'>
-             
-        </Col>
-      </Row>
-    </Container>
-    </section>
-  )
-}
+      <Container fluid id="home-bg">
+{/* adding video to the background of container */}
 
-export default Home
+        <Container>
+          <Row>
+            <Col className="col-lg-6 col-md-6 col-sm-12 mt-5">
+              <h1 className="text-lead">Modern CNC Solutions</h1>
+              <ul className="styled fs-5">
+                <li>New contemporary high-tech CNC tool machines</li>
+                <li>Globally advanced CAD/CAM software</li>
+                <li>
+                  Application of comprehensive design, production and control
+                  computerized systems.
+                </li>
+                <li>
+                  Creation of a fully equipped production control department
+                </li>
+              </ul>
+            </Col>
+            <Col className="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 d-none d-sm-block">
+              <img
+                alt=""
+                src="/images/CNC.jpg"
+                height="100"
+                className="img-fluid rounded-5"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container>
+        <Row>
+          <Col className="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5">
+            <ReactPlayer
+              url="\videos\cnc-machining-working-videos.mp4"
+              controls={true}
+              width={500}
+            ></ReactPlayer>
+          </Col>
+          <Col className="col-lg-6 col-md-6 col-sm-12 mt-5">
+            <h1 className="text-lead mt-5">CNC Milling and CNC Turning</h1>
+            <p className="fs-5">
+              CNC Milling and CNC Turning allow users to create patterns and add
+              detail to metals that are impossible to do by hand. CNC Milling
+              uses commands, codes programmed into the computer and set to run.
+              The mill then drills and turns along axes to cut materials to
+              dimensions entered into the computer.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col className="work"></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Home;
